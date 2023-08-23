830aa09441e354cf563fd789e779b10a91c4dc9d
ravi0604-singla <90888296+ravi0604-singla@users.noreply.github.com>
2023-08-23 14:22:59 +0530

    Update minimuminrotatedsortedarray.cpp

--- minimuminrotatedsortedarray.cpp
+++ minimuminrotatedsortedarray.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) 
+    // int findMin(vector<int>& nums) 
+    // {
+    //     if(nums.size() == 1)
+    //     return nums[0];
+    //     if(nums[0] < nums[nums.size() - 1])
+    //     return nums[0];
+    //     int low =0;
+    //     int high = nums.size() - 1;
+    //     while(low <= high)
+    //     {
+    //         int mid = low + (high - low)/2;
+    //         if(nums[0] <= nums[mid])
+    //         {
+    //         low = mid + 1;
+    //         }
+    //         else{
+    //         high = mid - 1;
+    //         }
+    //     }   
+    //     return nums[low];
+    // }
+int findMin(vector<int>&arr)
+{
+    int low = 0;
+    int n = arr.size();
+    int ans = INT_MAX;
+    int high = n - 1;
+    while(low <= high)
     {
-        if(nums.size() == 1)
-        return nums[0];
-        if(nums[0] < nums[nums.size() - 1])
-        return nums[0];
-        int low =0;
-        int high = nums.size() - 1;
-        while(low <= high)
+        int mid = (low + high)/2;
+        if(arr[low]<=arr[mid])
         {
-            int mid = low + (high - low)/2;
-            if(nums[0] <= nums[mid])
-            {
+            ans = min(ans, arr[low]);
             low = mid + 1;
-            }
-            else{
+        }
+        else
+        {
+            ans = min(ans, arr[mid]);
             high = mid - 1;
-            }
-        }   
-        return nums[low];
+        }
     }
+    return ans;
+}
 };
